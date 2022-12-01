237b98a567785f8b853c7d1b0ed96d0b3892a334
nathan6am <nathantpho@gmail.com>
2022-12-01 17:41:01 -0500

    basic auth

--- server/handlers/LobbyHandler.ts
+++ server/handlers/LobbyHandler.ts
@@ -3,4 +3,8 @@ const id = nanoid(12);
 import * as socketio from "socket.io";
 import redis from "@/util/db/redis";
 
-export default function (io: socketio.Server, socket: socketio.Socket): void {}
+export default function (io: socketio.Server, socket: socketio.Socket): void {
+    socket.on("lobby:create", (userid) => {
+        
+    })
+}
