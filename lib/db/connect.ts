40a36332ce2ed9e234748893e7c922c46b28f83a
nathan6am <nathantpho@gmail.com>
2023-01-23 01:05:37 -0500

    db connection fix

--- lib/db/connect.ts
+++ lib/db/connect.ts
@@ -1,7 +1,8 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User, Game, Puzzle, Notification, Analysis, User_Game, Credential } from "./entities/user";
-
+import { loadEnvConfig } from "@next/env";
+loadEnvConfig("./", process.env.NODE_ENV !== "production");
 const username = process.env.DB_USERNAME || "";
 const port = parseInt(process.env.DB_PORT || "5432");
 const password = process.env.DB_PASSWORD || "";
